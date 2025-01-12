684fe1fd9bc8fc0acfa51f8d1ea49f59bd172213
Sujaltalreja29 <sujaltlrj@gmail.com>
2025-01-12 21:28:04 +0530

    Added Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Logo({width = '100px'}) {
+function Logo({}) {
   return (
-    <div>Logo</div>
+    <div><img className='h-8 shadow-lg' src="src\img\Screenshot 2025-01-12 212444.png" alt="" /></div>
   )
 }
 
